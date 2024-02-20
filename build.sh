c8b95a7db393d0f8db6c36580c6e8438d37b100e
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-20 18:42:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,40 +1,56 @@
 #!/bin/bash
 
 set -e
-#Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
 
-# Run inside foss.crave.io devspace
+# Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
-crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
+# Initialize repo with specified manifest
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1 ;\
 
 # Clone local_manifests repository
-git clone https://github.com/sounddrill31/local_manifests --depth 1 -b lineage-oxygen .repo/local_manifests && \
+git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests ;\
 
- # Sync the repositories
+# Removals
+rm -rf prebuilts/clang/host/linux-x86/clang-latest external/chromium-webview && \
+
+# Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+
 
 # Set up build environment
 source build/envsetup.sh && \
 
 # Lunch configuration
-lunch lineage_oxygen-userdebug && \
+opt_patch ;\
+riseup X01BD userdebug ;\
 
-# Build the ROM
-mka bacon"
+croot ;\
+ascend ; \
+# echo "Date and time:" ; \
+
+# Print out/build_date.txt
+# cat out/build_date.txt; \
+
+# Print SHA256
+# sha256sum out/target/product/*/*.zip"
 
 # Clean up
-rm -rf oxygen 
+# rm -rf tissot/*
 
 
 
 # Pull generated zip files
-crave pull out/target/product/*/*.zip
+# crave pull out/target/product/*/*.zip
 
 # Pull generated img files
-crave pull out/target/product/*/*.img 
+# crave pull out/target/product/*/*.img
 
 # Upload zips to Telegram
-telegram-upload --to sdreleases oxygen/*.zip
+# telegram-upload --to sdreleases tissot/*.zip
+
+#Upload to Github Releases
+#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
