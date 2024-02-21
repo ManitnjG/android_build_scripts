5f0db1459bde33a3d15475e2a29330586ee177a3
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-21 05:59:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,48 +7,21 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs 
 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
-# Initialize repo with specified manifest
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1 ;\
+rm -rf .repo/local_manifests &&
+
 
 # Clone local_manifests repository
-git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests ;\
+git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests ;\
 
+if [ ! 0 == 0 ]
+then curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1 
+fi &&
 
 # Sync the repositories
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+repo sync -c -j4 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync &&
 
-
-# Set up build environment
-source build/envsetup.sh && \
-
-# Lunch configuration
-opt_patch ;\
-riseup X01BD userdebug ;\
-
-croot ;\
-ascend ; \
-# echo "Date and time:" ; \
-
-# Print out/build_date.txt
-# cat out/build_date.txt; \
-
-# Print SHA256
-# sha256sum out/target/product/*/*.zip"
-
-# Clean up
-# rm -rf tissot/*
-
-
-
-# Pull generated zip files
-# crave pull out/target/product/*/*.zip
-
-# Pull generated img files
-# crave pull out/target/product/*/*.img
-
-# Upload zips to Telegram
-# telegram-upload --to sdreleases tissot/*.zip
-
-#Upload to Github Releases
-#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
+# Set up build environment 
+export BUILD_USERNAME=ManitnjG && 
+export BUILD_HOSTNAME=crave &&
+export TZ=Asia/Jakarta &&
+source build/envsetup.sh && lunch lineage_X01BD-userdebug && make clean && mka bacon -j4
