6f959c6094641e51a8ffe44c725bd7364270c99b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-25 07:02:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,1 @@
-# Remove local_manifests directory
-rm -rf .repo/local_manifests
-rm -rf build
-# Clone local_manifests repository
-git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests
-# Clone crdroid 14
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs
-
-# repo sync
-repo sync -c -j14 --force-sync --no-clone-bundle --no-tags --prune
-
-# Set up build environment
-source build/envsetup.sh
-# Lunch configuration
-lunch lineage_X01BD-userdebug
-# Build confriguration
-make bacon -j14
+rm -rf .repo/local_manifests && rm -rf prebuilts && repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs && https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && repo sync -c -j 4 --force-sync && source build/envsetup.sh && export USE_CCACHE=1  && lunch lineage_X01BD-userdebug && make bacon
