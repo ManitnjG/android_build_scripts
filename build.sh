7c3bc4e023c7cfc4ad06ad0a4b458a7cfc8c7cd2
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-28 15:48:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,55 @@
- rm -rf .repo/local_manifests && repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs&&https://github.com/ManitnjG/local_manifest-1 -b los -c .repo/local_manifests && repo sync --force-sync -c -j 4 && source build/envsetup.sh && lunch lineage_X01BD-userdebug && mka bacon
+#!/bin/bash
+
+set -e
+
+# Initialize repo with specified manifest
+ repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+
+# Run inside foss.crave.io devspace, in the project folder
+# Remove existing local_manifests
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
+# Initialize repo with specified manifest
+repo init -u https://github.com/Evolution-X/manifest -b udc ;\
+
+# Clone local_manifests repository
+git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b evo .repo/local_manifests ;\
+
+# Removals
+rm -rf system/libhidl prebuilts/clang/host/linux-x86 prebuilt/*/webview.apk platform/external/python/pyfakefs platform/external/python/bumble external/chromium-webview/prebuilt/x86_64 platform/external/opencensus-java && \
+
+# Sync the repositories
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+
+
+# Set up build environment
+source build/envsetup.sh && \
+
+# Lunch configuration
+lunch evolution_X01BD-userdebug ;\
+
+croot ;\
+mka evolution ; \
+echo "Date and time:" ; \
+
+# Print out/build_date.txt
+cat out/build_date.txt; \
+
+# Print SHA256
+sha256sum out/target/product/*/*.zip"
+
+# Clean up
+# rm -rf tissot/*
+
 
 
 # Pull generated zip files
-crave pull out/target/product/*/*.zip 
+# crave pull out/target/product/*/*.zip 
 
 # Pull generated img files
-crave pull out/target/product/*/*.img
+# crave pull out/target/product/*/*.img
 
 # Upload zips to Telegram
-telegram-upload --to sdreleases oxygen/*.zip oxygen/recovery.img
+# telegram-upload --to sdreleases tissot/*.zip
 
 #Upload to Github Releases
-#cd oxygen/
 #curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
