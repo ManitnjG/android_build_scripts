67936cf1c39def74e95acaf78b950507b5085fe3
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-28 15:54:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -e
 
 # Initialize repo with specified manifest
- repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+
+crave set --projectID 72
 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
