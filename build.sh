45146f33416ea53af7cd1ce618e2bba12019edff
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-29 18:54:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,35 +2,28 @@
 
 set -e
 
-# Initialize repo with specified manifest
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 crave set --projectID 72
 
-# Run inside foss.crave.io devspace, in the project folder
-# Remove existing local_manifests
+
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
-# Initialize repo with specified manifest
+
 repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 
-# Sync the repositories
+
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
-
-# Set up build environment
 source build/envsetup.sh && \
 
-# Lunch configuration
-lunch evolution_X01BD-userdebug ;\
+lunch lineage_X01BD-userdebug ;\
 
 croot ;\
-m evolution ; \
+mka bacon -j16 ; \
 echo "Date and time:" ; \
 
-# Print out/build_date.txt
 cat out/build_date.txt; \
 
-# Print SHA256
 sha256sum out/target/product/*/*.zip"
 
 cd ~/work/out/target/product/X01BD
