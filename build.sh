73ba375ffe66b635ccbe61bd6d7b662b380c4c61
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-01 22:43:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ crave set --projectID 72
 
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 
+git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && \
+
+
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
 source build/envsetup.sh && \
