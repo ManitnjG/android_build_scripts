c517dda862050d8e3a527cf1672bf6ecc2517a16
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-02 07:41:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/PixelExperience/manifest -b fourteen
 
 crave set --projectID 72
 
@@ -16,7 +16,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 
 source build/envsetup.sh && \
 
-lunch lineage_X01BD-userdebug ;\
+lunch aosp_X01BD-userdebug ;\
 
 croot ;\
 mka bacon -j16 ; \
@@ -26,20 +26,12 @@ cat out/build_date.txt; \
 
 sha256sum out/target/product/*/*.zip"
 
-cd ~/work/out/target/product/X01BD  ;\
-wget https://github.com/Sushrut1101/GoFile-Upload/raw/master/upload.sh  ;\
-chmod +x upload.sh  ;\
-ls  ;\
-./upload.sh e*.zip  ;\
-./upload.sh boot.img  ;\
-
-
 
 # Pull generated zip files
-# crave pull out/target/product/*/*.zip 
+ crave pull out/target/product/*/*.zip 
 
 # Pull generated img files
-# crave pull out/target/product/*/*.img
+crave pull out/target/product/*/*.img
 
 # Upload zips to Telegram
 # telegram-upload --to sdreleases tissot/*.zip
