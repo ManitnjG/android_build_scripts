adeacfc98f85dc27844e60dfe850f3e19ab2ffcf
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-04 19:24:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 
 crave run --no-patch -- "rm -rf .repo/local_manifests ;\
+
 repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b evo .repo/local_manifests ;\
 
 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags ;\
+repo sync -c -j10 --force-sync --no-clone-bundle --no-tags ;\
 
 
 source build/envsetup.sh ;\
