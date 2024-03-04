120a36797ce293342b5296484575c0a07889e520
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-04 14:14:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,34 +7,29 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 
 crave set --projectID 72
 
-crave run --no-patch -- "rm -rf .repo .repo/local_manifests android art bionic bootable build cts dalvik developers development device external frameworks hardware kernel libcore libnativehelper lineage-sdk packages pdk platform platform_testing prebuilts sdk system test toolchain tools vendor && \
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
 
-export TZ='Asia/Jakarta' && \
+repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 
-repo init -u https://github.com/Evolution-X/manifest -b udc
+git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b evo .repo/local_manifests ;\
 
-git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b evo .repo/local_manifests && \
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
-export ALLOW_MISSING_DEPENDENCIES=true
 
 source build/envsetup.sh && \
+
 lunch evolution_X01BD-userdebug ;\
 
-mka bacon ; \
+croot ;\
+m evolution ; \
 echo "Date and time:" ; \
 
+# Print out/build_date.txt
 cat out/build_date.txt; \
 
-sha256sum out/target/product/*/*.zip
+crave pull out/target/product/*/*.zip 
 
-crave pull out/target/product/*/*.zip "
-# Pull generated zip files
- 
-
-# Pull generated img files
-crave pull out/target/product/*/*.img
 
 # Upload zips to Telegram
 # telegram-upload --to sdreleases tissot/*.zip
