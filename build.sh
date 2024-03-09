7dfef2ef123192a0757f79b7891ded62db479d3c
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-09 19:32:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 crave set --projectID 72
 
