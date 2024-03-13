8f6743964b92e42812eb1f710d6c710b3bf41720
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-13 14:33:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ crave run --clean --no-patch -- "rm -rf .repo/local_manifests
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b ris .repo/local_manifests && \
+ls && \
 source build/envsetup.sh ;\
 export ALLOW_MISSING_DEPENDENCIES=true ;\
 export RISING_PACKAGE_TYPE=VANILLA_AOSP ;\
