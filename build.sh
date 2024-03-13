55b3a969f9168c2258c63d6ab9e808108ab29947
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-13 21:32:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ crave set --projectID 72
 
 crave run --clean --no-patch -- "rm -rf .repo/local_manifests
 
+rm -rf android build device libnativehelper prebuilts vendor Android.bp BUILD external lineage sdk WORKSPACE art cts frameworks lineage-sdk system bionic dalvik hardware packages test bootable developers kernel pdk toolchain bootstrap.bash development libcore platform_testing tools && \
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b ris .repo/local_manifests && \
-ls && \
 source build/envsetup.sh ;\
 export ALLOW_MISSING_DEPENDENCIES=true ;\
 export RISING_PACKAGE_TYPE=VANILLA_AOSP ;\
