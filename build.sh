0e0391f559c9fde64f89fc5577a5751e3d8a0690
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-15 22:27:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 
 crave set --projectID 72
 
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
+crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b ris .repo/local_manifests && \
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
