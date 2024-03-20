a696b01c99d4ac57cbe4b927630d4d2563e9fe7a
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-20 08:06:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 
 crave run --no-patch -- " rm -rf device/asus/sdm660-common
-rm -rf device/asus/X01BD
-rm -rf vendor/asus
-rm -rf kernel/asus/sdm660
-git clone https://github.com/ManitnjG/android_device_asus_X01BD-4.19 -b lineage-21.0-RetrofitDynParts device/asus/X01BD
-git clone https://github.com/SonicBSV/android_device_asus_sdm660-common-4.19  -b lineage-21.0-RetrofitDynParts device/asus/sdm660-common
-git clone https://github.com/SonicBSV/proprietary_vendor_asus-4.19 -b lineage-21.0 vendor/asus
-git clone https://github.com/SonicBSV/android_kernel_asus_sdm660-4.19 -b master-RetrofitDynParts kernel/asus/sdm660
-
+git clone https://github.com/ManitnjG/android_device_asus_sdm660-common -b 14-4.19 device/asus/sdm660-common
 source build/envsetup.sh ;\
 
 export ALLOW_MISSING_DEPENDENCIES=true ;\
