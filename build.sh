4d7ab85c9bcfa30ddbdd3270dac46881dab015b7
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-20 08:52:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 
 crave run --no-patch -- " rm -rf .repo/local_manifests
+git clone https://github.com/ManitnjG/local_manifest-1 -b los 
 
 source build/envsetup.sh ;\
 
