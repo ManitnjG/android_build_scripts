f7ac6818862408146dd41b579cfc991e7539db3c
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-20 15:35:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ crave set --projectID 72
 crave run --no-patch -- " rm -rf .repo/local_manifests
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 
-git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests && \
+git clone https://github.com/ManitnjG/local_manifest-1 -b mat .repo/local_manifests && \
 
 /opt/crave/resync.sh && \
 
