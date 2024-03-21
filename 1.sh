6f8a8954614540888e149d8a357b8009d7742405
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-21 21:54:54 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+set -e
 
+repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
-rm -rf vendor/lineage
+crave set --projectID 72
+
+crave run --no-patch -- "rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
 
 source build/envsetup.sh ;\
@@ -9,4 +14,4 @@ export ALLOW_MISSING_DEPENDENCIES=true ;\
 
 lunch lineage_X01BD-userdebug ;\
 rm -rf out/target/product/X01BD/
-m bacon ;\
+m bacon ;\ "
