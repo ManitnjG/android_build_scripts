ef863b217e24b5dcdab85fde8c8e6199c506beba
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-21 06:59:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 
 crave set --projectID 72
 
-crave run --no-patch -- " rm -rf .repo/local_manifests /tmp/src/android  && \
+crave run --no-patch -- " rm -rf .repo/local_manifests && \
 
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs  && \
 
