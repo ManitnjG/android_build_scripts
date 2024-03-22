49dae8971a83fd232b10760088d1ae94dbb5890d
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-22 07:21:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 
 crave run --no-patch -- " rm -rf .repo/local_manifests && \
-
+sudo apt install git-lfs
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 
 git clone https://github.com/ManitnjG/local_manifest-1 -b ris .repo/local_manifests && \
@@ -15,7 +15,7 @@ git clone https://github.com/ManitnjG/local_manifest-1 -b ris .repo/local_manife
 
  rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
-
+purge ;\
 source build/envsetup.sh ;\
 
 export ALLOW_MISSING_DEPENDENCIES=true ;\
