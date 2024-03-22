185c8da0ca9046278ba8fc2665496da7cd237673
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-22 07:18:19 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,8 +4,10 @@ set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 crave set --projectID 72
-
 crave run --no-patch -- "rm -rf vendor/lineage
+
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
 
 source build/envsetup.sh ;\
