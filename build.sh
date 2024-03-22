cbf7d551584d4853ba199005c46b05d44f5949a0
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-22 17:32:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 
 crave set --projectID 72
 
-crave run --no-patch -- " rm -rf .repo/local_manifests && \
-sudo apt install git-lfs
+crave run --no-patch -- "rm -rf .repo/local_manifests/ prebuilts/clang/host/linux-x86  && \
+
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 
 git clone https://github.com/ManitnjG/local_manifest-1 -b ris .repo/local_manifests && \
