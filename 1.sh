99ecec26bee190f3f94ed50b2a989617e7e237bd
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-25 18:22:19 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -5,10 +5,10 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 
 crave set --projectID 72
 crave run --no-patch -- "rm -rf .repo/local_manifests android build device libnativehelper sdk WORKSPACE  Android.bp BUILD external lineage-sdk system art cts frameworks packages test bionic dalvik hardware pdk toolchain  bootable developers kernel platform_testing tools bootstrap.bash development libcore prebuilts vendor  &&    
-git clone https://github.com/ManitnjG/local_manifest-1 -b mat .repo/local_manifests &&
+git clone https://github.com/ManitnjG/local_manifest-1 -b nu .repo/local_manifests &&
 
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs && \
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 && \
+ 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16 && ls &&
- rm -rf vendor/lineage &&
-git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage &&
-source build/envsetup.sh && lunch lineage_X01BD-userdebug && export TZ=Asia/Kolkata && rm -rf out/target/product/X01BD/  &&  m bacon "
+
+source build/envsetup.sh && lunch nad_X01BD-userdebug && mka nad "
