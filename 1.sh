83a59609a379b0b3948bb2acb11c409b3eb1b2f8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-25 13:28:47 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,6 +4,6 @@ set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 crave set --projectID 72
-crave run --no-patch -- "rm -rf .repo/local_manifests && \ git clone https://github.com/ManitnjG/local_manifest-1 -b mat .repo/local_manifests && \ 
+crave run --no-patch -- "rm -rf .repo/local_manifests &&git clone https://github.com/ManitnjG/local_manifest-1 -b mat .repo/local_manifests &&
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs && \
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16 && \ source build/envsetup.sh && \ lunch lineage_X01BD-userdebug && \ export TZ=Asia/Kolkata && \ m bacon "
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16 &&source build/envsetup.sh &&lunch lineage_X01BD-userdebug &&export TZ=Asia/Kolkata &&m bacon "
