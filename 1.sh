07c2ccb2f49ae12b6b8a674b8d4cf72bc45b7cda
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-26 14:35:36 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -7,9 +7,8 @@ crave set --projectID 72
 crave run --no-patch -- "rm -rf .repo/local_manifests 
 git clone https://github.com/ManitnjG/local_manifest-1 -b nu .repo/local_manifests &&
 
-repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 && \
+repo init -u https://github.com/Havoc-OS-Revived/android_manifest.git -b eleven --git-lfs && \
  
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16 && ls &&
-rm -rf out/target/product/
-source build/envsetup.sh && lunch nad_X01BD-userdebug && mka nad -j16 &&
-wget https://gist.githubusercontent.com/ManitnjG/f26dc82fd4ce41c41229567ae5f3d841/raw/55f173db002de0571075b770382b060147ec73cf/fix_duplicates.sh && chmod +x fix_duplicates.sh && ./fix_duplicates.sh"
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16 &&
+export WITH_GAPPS=true
+source build/envsetup.sh && export WITH_GAPPS=true && lunch havoc_X01BD-userdebug && m bacon -j16 "
