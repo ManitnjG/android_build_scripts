264edf8bd765fed84b3bca76d8672dfe33c7a34f
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-26 10:31:48 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -12,4 +12,4 @@ repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 && \
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j16 && ls &&
 rm -rf out/target/product/
 source build/envsetup.sh && lunch nad_X01BD-userdebug && mka nad -j16 &&
-wget https://gist.githubusercontent.com/ManitnjG/f26dc82fd4ce41c41229567ae5f3d841/raw/55f173db002de0571075b770382b060147ec73cf/fix_duplicates.sh && chmod +x fix_duplicates.sh && ./fix_duplicates.sh "
+wget https://gist.githubusercontent.com/ManitnjG/f26dc82fd4ce41c41229567ae5f3d841/raw/55f173db002de0571075b770382b060147ec73cf/fix_duplicates.sh && chmod +x fix_duplicates.sh && ./fix_duplicates.sh"
