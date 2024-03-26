856fbdb65fc23cd0fcc3f88ff104123b3c25c66d
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-26 16:44:01 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,7 +4,7 @@ set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 crave set --projectID 72
-crave run --no-patch -- "rm -rf .repo/local_manifests 
+crave run --no-patch -- "rm -rf .repo/local_manifests android build device libnativehelper sdk WORKSPACE  Android.bp BUILD external lineage-sdk system art cts frameworks packages test bionic dalvik hardware pdk toolchain  bootable developers kernel platform_testing tools bootstrap.bash development libcore prebuilts vendor  &&  
 git clone https://github.com/ManitnjG/local_manifest-1 -b nu .repo/local_manifests &&
 
 repo init -u https://github.com/Havoc-OS-Revived/android_manifest.git -b eleven --git-lfs && \
