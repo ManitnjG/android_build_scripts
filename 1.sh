1d2b9b15076272b85bb0a31568b29f606679564f
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-29 19:51:53 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -7,7 +7,6 @@ crave set --projectID 72
 crave run --no-patch -- "rm -rf .repo/local_manifests Android.bp bootable compatibility developers external kernel Makefile packages prebuilts test vendor art bootstrap.bash cts development frameworks libcore manifest pdk sdk toolchain bionic build dalvik device hardware libnativehelper out platform_testing system tools &&
 
 git clone https://github.com/ManitnjG/local_manifest-1 -b nu .repo/local_manifests &&
-
-repo init --depth=1 -u https://github.com/Corvus-AOSP/android_manifest.git -b 11 && 
+repo init -u https://github.com/Havoc-OS-Revived/android_manifest.git -b eleven --git-lfs && 
 /opt/crave/resync.sh  && 
 source build/envsetup.sh && export WITH_GAPPS=false &&  lunch havoc_X01BD-userdebug && m bacon"
