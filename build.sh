6e449518579c352bb26fc8b5793078353576d1bf
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-03-30 14:54:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,31 +5,30 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 
 crave set --projectID 72
 
-crave run --no-patch -- "rm -rf .repo/local_manifests/  && \
+crave run --no-patch -- "rm -rf .repo/local_manifests/  &&
 
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
+repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen-aosp &&
 
-git clone https://github.com/ManitnjG/local_manifest-1 -b ris .repo/local_manifests && \
+git clone https://github.com/ManitnjG/local_manifest-1 -b sup .repo/local_manifests &&
 
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1 -b ris 
    echo Git clone failed, downloading through curl instead...
  fi 
 
-/opt/crave/resync.sh && \
+/opt/crave/resync.sh && 
 
 
- rm -rf vendor/lineage
-git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
-purge ;\
-source build/envsetup.sh ;\
+ rm -rf vendor/lineage &&
+git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage &&
 
-export ALLOW_MISSING_DEPENDENCIES=true ;\
 
-lunch rising_X01BD-userdebug ;\
-rm -rf out/target/product/X01BD/
-m ;\
-echo "Date and time:" ; \
+source build/envsetup.sh &&
+export ALLOW_MISSING_DEPENDENCIES=true &&
+lunch superior_X01BD-userdebug &&
+rm -rf out/target/product/X01BD/ &&
+mka bacon  &&
+echo "Date and time:" &&
 
 crave pull out/target/product/*/*.zip "
 
