f0f3a516077ff18e45fe58b4ed0db494ef6dec9b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-01 20:10:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,28 +6,18 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 
 crave run --no-patch -- "rm -rf .repo/local_manifests/  &&
-
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen &&
-
-git clone https://github.com/ManitnjG/local_manifest-1 -b sup .repo/local_manifests &&
-
-if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1 -b ris 
-   echo Git clone failed, downloading through curl instead...
- fi 
-
+repo init -u https://github.com/Evolution-X/manifest -b uqpr2 &&
+git clone https://github.com/ManitnjG/local_manifest-1 -b evo .repo/local_manifests &&
 /opt/crave/resync.sh && 
-
-
  rm -rf vendor/lineage &&
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage &&
 
 
 source build/envsetup.sh &&
 export ALLOW_MISSING_DEPENDENCIES=true &&
-lunch superior_X01BD-userdebug &&
-rm -rf out/target/product/X01BD/ &&
-mka bacon  &&
+lunch evolution_X01BD-userdebug &&
+
+m evolution  &&
 echo "Date and time:" &&
 
 crave pull out/target/product/*/*.zip "
