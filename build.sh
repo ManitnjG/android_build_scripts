7e1b55c48893f5f1c13a6b452b11379c84640140
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-06 21:49:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ crave set --projectID 72
 
 crave run --no-patch -- "rm -rf .repo/local_manifests/  &&
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs &&
-git clone https://github.com/ManitnjG/local_manifest-1 -b ris .repo/local_manifests &&
+git clone https://github.com/ManitnjG/local_manifest-1 -b rising .repo/local_manifests &&
 
 /opt/crave/resync.sh && 
  rm -rf vendor/lineage &&
