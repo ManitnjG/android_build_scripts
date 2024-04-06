49e54a8d9ad4d92100fad3b3e2068cf1628817a4
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-06 21:18:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 set -e
-mkdir lo
-cd lo
+
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 crave set --projectID 72
 
-crave run --no-patch -- "rm -rf .repo/local_manifests/ de  los  repo  rising &&
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs &&
-git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests &&
+crave run --no-patch -- "rm -rf .repo/local_manifests/  &&
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs &&
+git clone https://github.com/ManitnjG/local_manifest-1 -b ris .repo/local_manifests &&
 
 /opt/crave/resync.sh && 
  rm -rf vendor/lineage &&
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage &&
 source build/envsetup.sh &&
 export ALLOW_MISSING_DEPENDENCIES=true &&
-lunch lineage_X01BD-userdebug &&
+lunch riseup_X01BD-userdebug &&
 
 m bacon  &&
 echo "Date and time:" &&
