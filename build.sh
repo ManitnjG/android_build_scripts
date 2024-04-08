653e7f0f41e7fa7eaddbfba0f0f81d9bd49247e8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-08 22:08:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-
 crave set --projectID 72
 crave run --no-patch -- "rm -rf .repo/local_manifests
-
 git clone https://github.com/ManitnjG/local_manifest-1.git --depth 1 -b los .repo/local_manifests && 
-
 /opt/crave/resync.sh && 
-
  rm -rf vendor/lineage &&
 git clone https://github.com/ManitnjG/android_vendor_lineage1 -b lineage-21.0 vendor/lineage &&
-source build/envsetup.sh &&
-export ALLOW_MISSING_DEPENDENCIES=true &&
-lunch lineage_X01BD-ap1a-userdebug &&
-mka bacon  &&
+source build/envsetup.sh && export ALLOW_MISSING_DEPENDENCIES=true && lunch lineage_X01BD-ap1a-userdebug && mka bacon
 echo "Date and time:" &&  "
 
 #crave pull out/target/product/*/*.zip
