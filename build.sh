fc3095596000a5ec536946cd8002791c3441c6bf
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-11 08:08:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git clone https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution &&
 git clone https://github.com/ManitnjG/android_device_asus_X01BD-4.19 -b evo device/asus/X01BD
 git clone https://github.com/SonicBSV/android_device_asus_sdm660-common-4.19 -b lineage-21.0 device/asus/sdm660-common
 git clone https://github.com/SonicBSV/proprietary_vendor_asus-4.19 -b lineage-21.0 vendor/asus
-git clone https://github.com/SonicBSV/android_kernel_asus_sdm660-4.19 -b master kernel/asus/sdm660
+git clone https://github.com/SonicBSV/android_kernel_asus_sdm660-4.19 -b master kernel/asus/sdm660 &&
  
 source build/envsetup.sh && croot && export ALLOW_MISSING_DEPENDENCIES=true && lunch evolution_X01BD-ap1a-userdebug && m evolution "
 
