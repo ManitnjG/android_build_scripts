3b1cac9e0e3b46b1216c92e7a901dda6577418fe
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-11 21:50:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --repo-url=https://github.com/android/tools_repo.git --repo-branch=stable --git-lfs
 crave set --projectID 72
 crave run --no-patch -- "rm -rf .repo/local_manifests out prebuilts/clang/host/linux-x86 && 
-/opt/crave/resync.sh && 
-rm -rf vendor/evolution &&
-git clone https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution &&
-git clone https://github.com/ManitnjG/android_device_asus_X01BD-4.19 -b evo device/asus/X01BD
-git clone https://github.com/SonicBSV/android_device_asus_sdm660-common-4.19 -b lineage-21.0 device/asus/sdm660-common
-git clone https://github.com/SonicBSV/proprietary_vendor_asus-4.19 -b lineage-21.0 vendor/asus
-git clone https://github.com/SonicBSV/android_kernel_asus_sdm660-4.19 -b master kernel/asus/sdm660 &&
- 
-source build/envsetup.sh && croot && export ALLOW_MISSING_DEPENDENCIES=true && lunch evolution_X01BD-ap1a-userdebug && m evolution "
-
-#crave pull
+# Initialize local repository
+repo init -u https://github.com/Evolution-X/manifest -b u
+ git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b evo .repo/local_manifests && 
+if [ ! 0 == 0 ]
+ then   curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1
+ fi 
+  # Sync the repositories
+  /opt/crave/resync.sh  && \
+  
+  # Set up build environment
+  export BUILD_USERNAME=ManitnjG ; \
+  export BUILD_HOSTNAME=crave ; \
+  source build/envsetup.sh && \
+  lunch evolution_X01BD-userdebug && \
+  make installclean && \
+  mka bacon"
