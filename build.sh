82b6bc174a09ec3d0df6ead6330126d23b16a07a
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-12 08:01:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 crave run --no-patch -- "
   # Sync the repositories
-  /opt/crave/resync.sh  && \
-  croot
+  rm -rf vendor/evolution
+  git clone https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution && \
   # Set up build environment
   export BUILD_USERNAME=ManitnjG ; \
   export BUILD_HOSTNAME=crave ; \
   source build/envsetup.sh && \
+  croot
   lunch evolution_X01BD-ap1a-userdebug && \
   make installclean && \
   m evolution"
