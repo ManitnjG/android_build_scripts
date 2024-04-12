6a010732294301a465f6ff3b9460115fcaefb888
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-12 07:18:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 72
-crave run --no-patch -- "rm -rf .repo/local_manifests out prebuilts/clang/host/linux-x86 && 
-# Initialize local repository
-repo init -u https://github.com/Evolution-X/manifest -b u
- git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b evo .repo/local_manifests && 
-if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1
- fi 
+crave run --no-patch -- "
   # Sync the repositories
   /opt/crave/resync.sh  && \
   croot
