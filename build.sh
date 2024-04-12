f73233f77e82b8596fe81b2acdafbf9a15c32dbe
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-12 21:07:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 72
-crave run --no-patch -- "
+crave run --no-patch -- "rm -rf device/asus/X01BD device/asus/sdm660-common vendor/asus kernel/asus/sdm660 
+ 
+  
   # Sync the repositories
   rm -rf vendor/evolution
   git clone https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution && \
