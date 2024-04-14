593d58c24222a87345e54dc66346448a4b84ef0e
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-14 19:14:14 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,10 +4,17 @@ set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 crave set --projectID 72
-crave run --no-patch -- "rm -rf .repo/local_manifests Android.bp bootable compatibility developers external kernel Makefile packages prebuilts test vendor art bootstrap.bash cts development frameworks libcore manifest pdk sdk toolchain bionic build dalvik device hardware libnativehelper out platform_testing system tools &&
-
-git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests &&
-repo init -u https://github.com/Evolution-X/manifest -b u && 
-/opt/crave/resync.sh  && 
+crave run --no-patch -- "rm -rf .repo/local_manifests/  && # Clone local_manifests repository
+repo init -u https://github.com/Evolution-X/manifest -b u
+ git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests && 
+if [ ! 0 == 0 ]
+ then   curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1
+   echo Git clone failed, downloading through curl instead...
+ fi 
+ rm -rf vendor/evolution
+ git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution
+  export BUILD_USERNAME=ManitnjG ; \
+  export BUILD_HOSTNAME=crave ; \
+chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh && 
 source build/envsetup.sh && export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true  &&
 export ALLOW_MISSING_DEPENDENCIES=true  &&  lunch evolution_X01BD-userdebug && m evolution"
