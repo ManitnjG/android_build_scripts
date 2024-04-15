971d3ae0e3c9a243b3db1b2e7f68be6fdc0a9f57
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-15 19:42:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 crave run --no-patch -- "rm -rf  .repo/local_manifests out
 # Initialize local repository
-repo init -u https://github.com/Evolution-X/manifest -b u
+repo init -u https://github.com/Evolution-X/manifest -b u && 
 git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests ; \
  /opt/crave/resync.sh  && 
  rm -rf vendor/evolution
