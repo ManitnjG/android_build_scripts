9cba0abbfc422abcef5ddad1a1e2e80cd04959d8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-15 21:46:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 72
-crave run --no-patch -- "
+crave run --no-patch -- "rm -rf out
  rm -rf vendor/evolution && 
  git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
   export BUILD_USERNAME=ManitnjG ; \
