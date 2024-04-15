c0bda67b60b1d54f14c5e541c7763d957c9a7178
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-15 07:52:03 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -6,7 +6,7 @@ crave run --no-patch -- "rm -rf device/asus/sdm660-common android_device_asus_sd
  git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution
   export BUILD_USERNAME=ManitnjG ; \
   export BUILD_HOSTNAME=crave ; \
-  git clone https://github.com/AnandSuresh02/android_device_asus_sdm660-common-4.19 -b fourteen device/asus/sdm660-common
+  git clone https://github.com/SKetU-l/device_asus_sdm660-common-4.19 -b fourteen device/asus/sdm660-common
 chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh && 
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true  &&
 export ALLOW_MISSING_DEPENDENCIES=true  && source build/envsetup.sh && lunch evolution_X01BD-userdebug && m evolution"
