e2233e929fd3dc95d7a73ed8a60bad707d74594b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-15 21:25:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ crave run --no-patch -- "rm -rf  .repo/local_manifests out
 repo init -u https://github.com/Evolution-X/manifest -b u && 
 git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests ; \
  /opt/crave/resync.sh  && 
- rm -rf vendor/evolution
- git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution
+ rm -rf vendor/evolution && 
+ git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
   export BUILD_USERNAME=ManitnjG ; \
   export BUILD_HOSTNAME=crave ; \
   source build/envsetup.sh && \
