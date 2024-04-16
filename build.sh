be56854ffac4f578b06cf3b553ae53bf8a587217
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-16 20:13:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,45 +12,11 @@ crave run --no-patch -- "rm -rf .repo/local_manifests && \
 repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 
 # Clone local_manifests repository
-git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b evo .repo/local_manifests ;\
+git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests ;\
 
 
 # Sync the repositories
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh && 
 
-
-# Set up build environment
-source build/envsetup.sh && \
-
-# Lunch configuration
-lunch evolution_X01BD-userdebug ;\
-
-croot ;\
-m evolution ; \
-echo "Date and time:" ; \
-
-# Print out/build_date.txt
-cat out/build_date.txt; \
-
-crave pull out/target/product/*/*.zip 
-
-
-# Print SHA256
-sha256sum out/target/product/*/*.zip"
-
-# Clean up
-rm -rf X01BD/*
-
-
-
-# Pull generated zip files
-crave pull out/target/product/*/*.zip 
-
-# Pull generated img files
-crave pull out/target/product/*/*.img
-
-# Upload zips to Telegram
-# telegram-upload --to sdreleases tissot/*.zip
-
-#Upload to Github Releases
-#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true  &&
+export ALLOW_MISSING_DEPENDENCIES=true  && source build/envsetup.sh && lunch evolution_X01BD-userdebug && m evolution"
