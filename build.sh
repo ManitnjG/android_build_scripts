24969f67035c7117513f0306eefea58219a92ca4
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-16 08:53:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,7 @@
 set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 72
-crave run --no-patch -- "rm -rf .repo/local_manifests
-repo init -u https://github.com/Evolution-X/manifest -b u  ; \
-git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests ; \
-rm -rf vendor/evolution
- git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
- /opt/crave/resync.sh  &&  
-  export BUILD_USERNAME=ManitnjG ; \
-  export BUILD_HOSTNAME=crave ; \
-  source build/envsetup.sh && \
+crave run --no-patch -- "source build/envsetup.sh && \
   croot
   lunch evolution_X01BD-userdebug && \
   make installclean && \
