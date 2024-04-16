40f0a7a1a019b7b001edc7a00ca94b26538c1824
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-16 07:27:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 crave set --projectID 72
- repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b u && 
 crave set --projectID 72
 crave run --no-patch -- "git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests ; \
  git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
