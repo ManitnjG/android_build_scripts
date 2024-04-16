52595590a5446c14e50c62ff95fb4725501ec247
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-16 06:57:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 crave set --projectID 72
-crave run --no-patch -- " rm -rf vendor/evolution  out && 
+ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+crave set --projectID 72
+crave run --no-patch -- "rm -rf Android.bp bootable compatibility developers external kernel Makefile packages prebuilts test vendor art bootstrap.bash cts development frameworks libcore manifest pdk sdk toolchain bionic build dalvik device hardware libnativehelper out platform_testing system tools &&
+crave run --no-patch -- "repo init -u https://github.com/Evolution-X/manifest -b u && 
+git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests ; \
  git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
+ /opt/crave/resync.sh  &&  
   export BUILD_USERNAME=ManitnjG ; \
   export BUILD_HOSTNAME=crave ; \
   source build/envsetup.sh && \
