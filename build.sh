8538a7e88036b154632b8d73c95037a4324fc562
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-16 20:29:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,10 @@
-#!/bin/bash
 
 set -e
 crave set --projectID 72
-# Initialize repo with specified manifest
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-
-# Run inside foss.crave.io devspace, in the project folder
-# Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
-# Initialize repo with specified manifest
 repo init -u https://github.com/Evolution-X/manifest -b udc ;\
-
-# Clone local_manifests repository
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests ;\
-
-
-# Sync the repositories
 chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh && 
  rm -rf vendor/evolution 
  git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
