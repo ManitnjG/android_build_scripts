62cc10df94920c41aff3a8588cf65e10d0dc16d0
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-16 06:51:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 set -e
+crave set --projectID 72
 crave run --no-patch -- " rm -rf vendor/evolution  out && 
  git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
   export BUILD_USERNAME=ManitnjG ; \
