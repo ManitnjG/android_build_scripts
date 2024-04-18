b5e3996b36f1ad720c47e269f4eaab522d409733
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-18 07:58:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ repo init -u https://github.com/Evolution-X/manifest -b u ;\
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests ;\
 chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh && 
  rm -rf vendor/evolution 
- git clone https://github.com/Evolution-X/vendor_evolution -b u vendor/evolution && 
+ git clone https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution && 
 
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true  &&
 export ALLOW_MISSING_DEPENDENCIES=true  && source build/envsetup.sh && lunch evolution_X01BD-userdebug && m evolution"
