9f46420e0faa70bea5d7e620bb0daca7f822a085
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-18 07:22:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 72
-crave run --no-patch -- "source build/envsetup.sh && \
+crave run --no-patch -- " rm -rf out
+  source build/envsetup.sh && \
   croot
   lunch evolution_X01BD-userdebug && \
   make installclean && \
