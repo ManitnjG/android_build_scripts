573a9a455a40c0b20748db37d8b7a2347fd2f9f1
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-18 07:58:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 72
-crave run --no-patch -- " rm -rf out vendor/evolution
+crave run --no-patch -- " rm -rf vendor/evolution out
   https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution && \
   source build/envsetup.sh && \
   croot
