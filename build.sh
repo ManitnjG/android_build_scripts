57e269437ca48dafb5631decd1cf5cf5ade3b332
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-20 08:02:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 
 crave set --projectID 72
 crave run --clean --no-patch -- "
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-
+chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh &&
 rm -rf vendor/lineage
   https://github.com/ManitnjG/vendor_lineage -b u vendor/lineage && \
   source build/envsetup.sh && \
