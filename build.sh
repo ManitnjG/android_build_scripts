4cd2c9ca04f9920a4c601d3205620020ab49644b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-20 08:24:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@ set -e
 crave set --projectID 72
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
-repo init -u https://github.com/Evolution-X/manifest -b u ;\
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests ;\
-chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh && 
- rm -rf vendor/evolution 
- git clone https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution && 
- source build/envsetup.sh  &&
-export TARGET_USES_MINI_GAPPS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true  &&
-export ALLOW_MISSING_DEPENDENCIES=true  && lunch evolution_X01BD-userdebug && m evolution"
+ repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh &&
+rm -rf vendor/lineage
+  https://github.com/ManitnjG/vendor_lineage -b u vendor/lineage && \
+  source build/envsetup.sh && \
+  croot
+  lunch rising_X01BD-userdebug && \
+  make installclean && \
+  m"
