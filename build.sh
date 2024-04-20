0cd9b3fd54a4433b2877570062a00abfbdaa5005
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-20 08:01:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,13 @@
 set -e
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 72
-crave run --clean --no-patch -- " rm -rf vendor/evolution out
-  https://github.com/ManitnjG/vendor_evolution -b u vendor/evolution && \
+crave run --clean --no-patch -- "
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+
+rm -rf vendor/lineage
+  https://github.com/ManitnjG/vendor_lineage -b u vendor/lineage && \
   source build/envsetup.sh && \
   croot
-  lunch evolution_X01BD-userdebug && \
+  lunch rising_X01BD-userdebug && \
   make installclean && \
-  m evolution"
+  mka bacon"
