96039dea2f9820875505eee384f2ae0d33cbab96
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-20 12:05:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ crave run --no-patch -- "rm -rf .repo/local_manifests && \
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b ris .repo/local_manifests ;\
  repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh &&
-rm -rf vendor/lineage
-  https://github.com/ManitnjG/vendor_lineage -b u vendor/lineage && \
   source build/envsetup.sh && \
   croot
   lunch rising_X01BD-userdebug && \
