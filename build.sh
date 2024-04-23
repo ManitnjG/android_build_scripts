7a71f4d34d91465f6440c4849b305c5f1608f6d4
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-23 20:53:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
-rm -rf .repo/local_manifests prebuilts/clang/host/linux-x86 && \
-git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b ris .repo/local_manifests ;\
- repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
- repo sync -j 10 &&
-  rm -rf vendor/lineage
+rm -rf vendor/lineage
   git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/lineage ;\
   source build/envsetup.sh && \
   croot
