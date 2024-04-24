f663a1e271c5bafc823ec8cce1e393cdf7f60918
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-24 14:22:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ rm -rf vendor/lineage &&
 git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/lineage &&
   source build/envsetup.sh &&
   croot
-  lunch riseup X01BD userdebug &&
+  riseup X01BD userdebug &&
   rise b #"
