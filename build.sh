2bff20457b2ed27be701755e88fa6cf139445272
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-24 19:31:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr &&
-crave set --projectID 79 &&
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
+crave set --projectID 79 
 crave run  --no-patch " rm -rf .repo/local_manifests && 
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b cip .repo/local_manifests && 
 chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh &&
