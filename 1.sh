1ff9e08a66358082ff72d947949066a610327b3b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-24 06:52:01 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
- rm -rf .repo/local_manifests prebuilts/clang/host/linux-x86 && \
+"rm -rf .repo/local_manifests prebuilts/clang/host/linux-x86 && \
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b ris .repo/local_manifests ;\ 
 repo init -u https://github.com/RisingTechOSS/android -b fourteen
-repo sync
+repo sync"
