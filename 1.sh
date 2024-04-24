a545650501d25a1c8a31209a680190d3c6e8003e
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-24 06:57:05 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
-sudo apt install repo
-repo init -u https://github.com/RisingTechOSS/android -b fourteen
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)"
+sudo apt install repo &&
+repo init -u https://github.com/RisingTechOSS/android -b fourteen &&
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all) 
