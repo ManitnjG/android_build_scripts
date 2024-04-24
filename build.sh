8f207d9adf309ae4c481fe1bab06be6640122b5b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-24 14:29:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#set -e
-#repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-#crave set --projectID 72
-#crave run  --no-patch " 
-rm -rf .repo/local_manifests prebuilts/clang/host/linux-x86 && 
+set -e
+repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+crave set --projectID 72
+crave run  --no-patch " rm -rf .repo/local_manifests prebuilts/clang/host/linux-x86 && 
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b ris .repo/local_manifests && 
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && 
 chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh &&
@@ -12,4 +11,4 @@ git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/l
   source build/envsetup.sh &&
   croot
   riseup X01BD userdebug &&
-  rise b #"
+  rise b "
