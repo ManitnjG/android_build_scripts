39a0366e2b7d7a42aa1b2bda7ef09d7fe5ae7456
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-25 06:31:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -e
 repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
 crave set --projectID 79 
 crave run  --no-patch " rm -rf .repo/local_manifests && 
-git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b cip .repo/local_manifests && 
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && 
 chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh &&
 source build/envsetup.sh &&
-lunch cipher_X01BD-ap1a-userdebug &&
-mka bacon "
+lunch lineage_X01BD-ap1a-userdebug &&
+rise b "
