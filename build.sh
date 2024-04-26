a1dbc88fe3cbc3cff2020293d6726b2bb2703bae
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-26 12:57:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-#git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && 
-#chmod +x /opt/crave/resync.sh && /opt/crave/resync.sh &&
+git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && 
 rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineag.git -b fourteen vendor/lineage
 source build/envsetup.sh &&
