e141bba230fc958e7f4d2281440a07a4bbe92fb7
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-26 16:02:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
 crave run  --no-patch " rm -rf .repo/local_manifests  out && 
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && 
+/opt/crave/resync.sh && 
 rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineag.git -b fourteen vendor/lineage
 source build/envsetup.sh &&
