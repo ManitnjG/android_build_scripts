94fbd5c4cbd42394cfa06af1163c5e978742508b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-27 05:22:16 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,5 @@
-source build/envsetup.sh &&
-lunch lineage_X01BD-ap1a-userdebug &&
-rise b 
+wget https://github.com/Sushrut1101/GoFile-Upload/raw/master/upload.sh
+chmod +x upload.sh
+ls
+./upload.sh *zip
+./upload.sh boot.img
