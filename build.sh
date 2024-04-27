42fb4bf5bf13735293082e0a1952b0f91c41ca85
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-27 16:45:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ crave run --no-patch -- "rm -rf .repo/local_manifests  out &&
 repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && 
 /opt/crave/resync.sh &&
-rm -rf vendor/lineage
+rm -rf vendor/sakura
 git clone https://github.com/ManitnjG/vendor_sakura -b 14 vendor/sakura
 . build/envsetup.sh &&
 lunch lineage_X01BD-ap1a-userdebug &&
