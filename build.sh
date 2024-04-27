e3ae85fb62e343841b880484b39a6bcd45e71253
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-27 16:21:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
 crave run --no-patch -- "rm -rf .repo/local_manifests  out && 
 repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && 
-
+/opt/crave/resync.sh &&
 rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/vendor_sakura -b 14 vendor/sakura
 source build/envsetup.sh &&
