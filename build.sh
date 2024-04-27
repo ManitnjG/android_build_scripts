f53184d81e01fee73719c94603c7f88509e16079
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-27 11:30:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
-#set -e
-#crave set --projectID 79
-#repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
+set -e
+crave set --projectID 79
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
 rm -rf .repo/local_manifests  out && 
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b los .repo/local_manifests && 
 /opt/crave/resync.sh && 
 rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineag.git -b fourteen vendor/lineage
 source build/envsetup.sh &&
-export RISING_MAINTAINER="ManiGtnj" &&
 lunch lineage_X01BD-ap1a-userdebug &&
-rise b
+mka bacon
