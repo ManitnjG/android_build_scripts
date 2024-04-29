7022e8435bd5aa7328d137c192030382e254556b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-29 18:18:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,2 @@
-set -e
-crave set --projectID 79
-repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
-crave run --no-patch -- "rm -rf .repo/local_manifests  out && 
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
-git clone https://github.com/ManitnjG/local_manifest-1 --depth 1 -b mat .repo/local_manifests && 
-/opt/crave/resync.sh &&
-rm -rf vendor/lineage
-git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
-. build/envsetup.sh &&
+crave run --no-patch -- ". build/envsetup.sh &&
 brunch X01BD"
