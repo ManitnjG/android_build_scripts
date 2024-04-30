42f1537aa893266b6a0901541955d76e31857692
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-30 15:39:36 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
-crave set --projectID 79
-repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
-crave run --no-patch -- "rm -rf .repo/local_manifests  out && 
+#crave set --projectID 79
+#repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
+#crave run --no-patch -- "rm -rf .repo/local_manifests  out && 
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests &&
 repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs &&
 /opt/crave/resync.sh &&
