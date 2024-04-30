6a421710255b8fbf5314b7847fa7f8949ed42774
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-30 20:16:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ set -e
 crave set --projectID 72
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave run --no-patch -- "rm -rf .repo/local_manifests  out && 
-git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests &&
 repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs &&
+git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests &&
 /opt/crave/resync.sh &&
 rm -rf vendor/voltage  &&
 git clone https://github.com/ManitnjG/vendor_voltage -b 14 vendor/voltage &&
