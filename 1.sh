507b514e136800cc4c17f9a456066d6597c20c7b
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-02 06:39:03 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,5 +3,5 @@ repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
 rm -rf .repo/local_manifests  out && 
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests &&
 repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs &&
-/opt/crave/resync.sh &&
+/opt/crave/resync.sh 
 
