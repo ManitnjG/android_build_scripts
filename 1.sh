e2672ab0033cd110ffddf5e1a27dc75b8f0d5bd3
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-02 06:39:47 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,4 +4,3 @@ rm -rf .repo/local_manifests  out &&
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
 repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs 
 /opt/crave/resync.sh 
-
