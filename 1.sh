f5610f662e1d53faf60df09fbe41b8ac10422e01
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-02 06:36:54 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,9 +2,6 @@ crave set --projectID 79
 repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
 crave run --no-patch -- "rm -rf .repo/local_manifests  out && 
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests &&
-repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs &&
+repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs &&
 /opt/crave/resync.sh &&
-rm -rf vendor/voltage  &&
-git clone https://github.com/ManitnjG/vendor_voltage -b 14 vendor/voltage &&
-. build/envsetup.sh &&
-brunch X01BD && "
+
