4cd50fe518c89b6f8c66a4b6ff0b45e9ef1cb3df
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-02 21:54:08 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,2 @@
-crave set --projectID 72
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-rm -rf .repo/local_manifests && 
-git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs 
-/opt/crave/resync.sh 
+crave run --no-patch -- " 	. build/envsetup.sh
+        brunch X01BD"
