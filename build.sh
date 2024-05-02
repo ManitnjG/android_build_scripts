cb3c34971df9b0a37338533e37263af695323164
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-02 06:40:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-crave run --no-patch -- "rm -rf out .repo 
-. build/envsetup.sh 
-brunch X01BD "
+crave set --projectID 79
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
+rm -rf .repo/local_manifests  out && 
+git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
+repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs 
+/opt/crave/resync.sh 
