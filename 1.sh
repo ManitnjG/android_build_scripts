5efb7958e60b0cd008a0650f70f8204c4bb6cae8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-03 22:24:50 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-crave run --no-patch -- "curl -sf  https://raw.githubusercontent.com/ManitnjG/android_build_scripts/plrOS-oxygen/build.sh | sh"
+crave run --no-patch -- "curl -sf  https://raw.githubusercontent.com/ManitnjG/android_build_scripts/plrOS-oxygen/build.sh | sh "
