1ca3b624db2d73f6586952e34de8c52e2727e1aa
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-03 07:52:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr 
 rm -rf .repo/local_manifests  out && 
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs  &&
+repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs
 /opt/crave/resync.sh 
+. build/envsetup.sh
+        brunch X01BD"
