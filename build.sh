7006c6e0ba2614322e14b48f26f311c46061b3d4
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-04 15:25:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-crave run --no-patch -- rm -rf .repo/local_manifests &&
+crave run --no-patch -- " rm -rf .repo/local_manifests &&
 repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
 if [ ! 0 == 0 ]
@@ -9,4 +9,4 @@ export BUILD_USERNAME=Mani
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 . build/envsetup.sh
-brunch X01BD
+brunch X01BD "
