6c46d55f9bf60e19f877b6d434e496a24c3c5403
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-05 02:40:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
-crave run --no-patch -- " rm -rf .repo/local_manifests &&
-repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs
-git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
-if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1.git
- fi
-/opt/crave/resync.sh
-export BUILD_USERNAME=Mani
-export BUILD_HOSTNAME=crave
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-. build/envsetup.sh
-brunch X01BD "
+crave run --no-patch -- ". build/envsetup.sh
+lunch lineage_X01BD-ap1a-userdebug
+mka bacon"
