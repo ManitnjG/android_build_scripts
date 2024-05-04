aca7955b01a2f44e37960f26ed18413771d564d1
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-04 15:21:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+
 rm -rf .repo/local_manifests &&
 repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
@@ -5,5 +6,8 @@ if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/ManitnjG/local_manifest-1.git
  fi
 /opt/crave/resync.sh
+export BUILD_USERNAME=Mani
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 . build/envsetup.sh
 brunch X01BD
