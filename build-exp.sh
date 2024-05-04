31d37d51ee03a508d9cd1f6b1eac5e4653195a24
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-04 21:19:18 +0530

    Update build-exp.sh

--- build-exp.sh
+++ build-exp.sh
@@ -1,3 +1,5 @@
-crave run --no-patch -- "  . build/envsetup.sh
+crave run --no-patch -- " rm -rf vendor/superior
+git clone https://github.com/ManitnjG/android_vendor_superior -b fourteen vendor/superior
+. build/envsetup.sh
   lunch superior_X01BD-ap1a-userdebug
   mka bacon "
