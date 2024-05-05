ef51da9e5f2a023a5155e6639507b47d104e4a36
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-05 10:11:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-crave run --no-patch -- "rm -rf vendor/voltage 
+crave run --no-patch -- "repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs
+
+
+rm -rf vendor/voltage 
 git clone https://github.com/ManitnjG/vendor_voltage -b 14 vendor/voltage 
 . build/envsetup.sh
 brunch X01BD"
