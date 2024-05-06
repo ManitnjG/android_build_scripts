6b707ea73f236a6b477ac79ac4caec1e5cbc3be8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-06 14:55:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
-crave run --no-patch -- "rm -rf .repo
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs
-git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
-/opt/crave/resync.sh &&
-rm -rf vendor/superior
-git clone https://github.com/ManitnjG/android_vendor_superior -b fourteen vendor/superior
+crave run --no-patch -- "rm -rf vendor/lineage
+git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
   . build/envsetup.sh
-  lunch superior_X01BD-ap1a-userdebug
-  mka bacon"
+  brunch X01BD"
