2d3e7cd058a3db46dd653056feea638913572c13
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-07 21:21:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-crave run --no-patch -- " rm -rf vendor/lineage
+crave run --no-patch -- " rm -rf .repo/local_manifests
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests 
+/opt/crave/resync.sh &&
+rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
+rm -rf packages/apps/Trebuchet packages/overlays/Lineage/icons
+
 source build/envsetup.sh
 brunch X01BD "
