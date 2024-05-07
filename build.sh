9af82d1d45ef157ae583ef8aaeeeb395b1cbcca1
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-07 07:39:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 crave run --no-patch -- "rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true 
   . build/envsetup.sh
   brunch X01BD"
