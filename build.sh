fecf072646155a524e533d86b14e3e07cb5a3767
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-07 16:51:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-crave run --no-patch -- "rm -rf vendor/lineage
+crave run --no-patch -- " rm -rf vendor/lineage
 git clone https://github.com/ManitnjG/android_vendor_lineage -b 14.0 vendor/lineage
-. build/envsetup.sh
-brunch X01BD"
+source build/envsetup.sh
+brunch X01BD "
