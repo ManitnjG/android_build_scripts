dcb068c0d01d6cb30da62b7462874323369f4e22
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-08 22:05:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 crave run --no-patch -- " rm -rf .repo/local_manifests
-repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 -b evo .repo/local_manifests 
 /opt/crave/resync.sh &&
 rm -rf vendor/evolution
