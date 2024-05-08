6f57918c54cc5e6e32aa4b1a994cf5d6633cb9c7
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-08 22:23:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-crave run --no-patch -- " rm -rf .repo/local_manifests
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-git clone https://github.com/ManitnjG/local_manifest-1 -b evo .repo/local_manifests 
-/opt/crave/resync.sh &&
-rm -rf vendor/evolution
-git clone https://github.com/ManitnjG/vendor_evolution -b udc vendor/evolution
-source build/envsetup.sh
-lunch lineage_X01BD-userdebug
-lunch lineage_X01BD-ap1a-userdebug
-m evolution "
+crave run --no-patch -- " rm -rf .repo/local_manifests && \
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
+git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests && \
+/opt/crave/resync.sh && \
+rm -rf vendor/lineage && \
+git clone https://github.com/ManitnjG/vendor_lineag -b fourteen vendor/lineage && \
+export PRODUCT_NO_CAMERA=true && export TARGET_CORE_GMS=true && \
+source build/envsetup.sh && \
+(lunch lineage_X01BD-userdebug || lunch lineage_X01BD-ap1a-userdebug) && \
+rise b "
