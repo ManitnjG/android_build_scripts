314c976a897882b6262a4fdd276525e8a3a01796
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-09 07:26:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests && \
 /opt/crave/resync.sh && \
 rm -rf vendor/lineage && \
-git clone https://github.com/ManitnjG/vendor_lineag -b fourteen vendor/lineage && \
+git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/lineage && \
 export PRODUCT_NO_CAMERA=true && export TARGET_CORE_GMS=true && \
 source build/envsetup.sh && \
 (lunch lineage_X01BD-userdebug || lunch lineage_X01BD-ap1a-userdebug) && \
