95596a7db0f68f289edacbb54e0ac9fd2e4d25de
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-10 21:08:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 crave run --no-patch -- " rm -rf .repo/local_manifests && \
 repo init -u https://github.com/SuperiorOS/manifest.git -b eleven && \
-git clone https://github.com/ManitnjG/local_manifest-1 -b sup .repo/local_manifests && \
+git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
 lunch superior_X01BD-userdebug && \
