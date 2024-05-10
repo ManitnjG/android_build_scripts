70f27f111ac8b1c9a8a6ecea5c8f61b20fb61300
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-10 07:04:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 crave run --no-patch -- " rm -rf .repo/local_manifests && \
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
-git clone https://github.com/ManitnjG/local_manifest-1 -b los .repo/local_manifests && \
+repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs && \
+git clone https://github.com/ManitnjG/local_manifest-1 -b sup .repo/local_manifests && \
 /opt/crave/resync.sh && \
-rm -rf vendor/lineage && \
-git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/lineage && \
-export PRODUCT_NO_CAMERA=true && export TARGET_CORE_GMS=true && \
+rm -rf vendor/superior && \
+git clone https://github.com/ManitnjG/android_vendor_superior -b fourteen vendor/superior && \
 source build/envsetup.sh && \
-lunch lineage_X01BD-ap1a-userdebug && \
-rise b "
+lunch superior_X01BD-ap1a-userdebug && \
+mka bacon "
