16b587db7297f207cf8fd492f9979c58bab8da08
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-11 22:45:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 crave run --no-patch -- " rm -rf .repo/local_manifests && \
-repo init -u https://github.com/SuperiorOS/manifest.git -b eleven && \
+repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs && \
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true 
 source build/envsetup.sh && \
-lunch superior_X01BD-userdebug && \
-mka bacon "
+brunch X01BD "
