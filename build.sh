003e9de1f963bd125adf57b65ef2e39b6a817f18
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-12 14:48:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-crave run --no-patch -- "source build/envsetup.sh && \
+crave run --no-patch -- "rm -rf vendor/voltage
+git clone https://github.com/ManitnjG/vendor_voltage.git -b 14 vendor/voltage
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true 
+source build/envsetup.sh && \
+source build/envsetup.sh && \
 brunch X01BD "
