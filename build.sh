7d559f6cb342fc5fe4f6ce39ef505302c4ac70ca
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-16 20:58:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@ crave run --no-patch -- "rm -rf .repo/local_manifests
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests && \
 /opt/crave/resync.sh && \
-rm -rf vendor/rising && \
-git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/rising && \
+rm -rf vendor/lineage && \
+rm -rf vendor/rising
+git clone https://github.com/RisingTechOSS/android_vendor_rising -b fourteen vendor/rising
+git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/lineage && \
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 export TARGET_CORE_GMS=true && \
