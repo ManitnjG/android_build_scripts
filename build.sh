8292ec6b64844741748319fa03a8f168c14bb37d
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-18 10:33:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 crave run --no-patch -- "rm -rf .repo/local_manifests
-repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR2 --git-lfs && \
-git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests && \
+repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR2 --git-lfs
+git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests
 /opt/crave/resync.sh && \
 rm -rf vendor/infinity
 git clone https://github.com/ManitnjG/vendor_infinity -b QPR2 vendor/infinity
