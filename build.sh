7fa5048f2c8982c4441d3e2e9ef2eb2ca46faf2f
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-18 06:48:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 crave run --no-patch -- "rm -rf .repo/local_manifests
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
+repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR2 --git-lfs && \
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests && \
 /opt/crave/resync.sh && \
-rm -rf vendor/lineage && \
-rm -rf vendor/rising
-git clone https://github.com/RisingTechOSS/android_vendor_rising -b fourteen vendor/rising
-git clone https://github.com/ManitnjG/android_vendor_lineag -b fourteen vendor/lineage && \
+rm -rf vendor/infinity
+git clone https://github.com/ManitnjG/vendor_infinity -b QPR2 vendor/infinity
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
-export TARGET_CORE_GMS=true && \
 source build/envsetup.sh && \
-lunch lineage_X01BD-ap1a-userdebug
-rise b"
+lunch infinity_X01BD-userdebug
+mka bacon"
