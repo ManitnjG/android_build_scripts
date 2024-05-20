45f92d4e3342834510bbc8eff566a22e94b245b7
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-20 19:31:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 crave run --no-patch -- "rm -rf .repo/local_manifests
-repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR2 --git-lfs
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests
 /opt/crave/resync.sh && \
-rm -rf vendor/infinity
-git clone https://github.com/ManitnjG/vendor_infinity -b QPR2 vendor/infinity
+rm -rf vendor/lineage
+git clone https://github.com/ManitnjG/vendor_lineage -b alpha-14 vendor/lineage
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
