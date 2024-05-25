d1639810d9c7b1dab462cd39bf0e4c8c0d1e8d45
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-25 11:25:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 crave run --no-patch -- "rm -rf .repo/local_manifests out
   repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11
 git clone https://github.com/ManitnjG/local_manifest-1 -b nad .repo/local_manifests
-/opt/crave/resync.sh && \
+repo sync
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
