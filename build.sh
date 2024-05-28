abac7a4401368dfad57ea8b6cb09e15635131d38
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-28 10:24:13 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifes
 /opt/crave/resync.sh && \
 rm -rf vendor/evolution
 git clone https://github.com/ManitnjG/vendor_evolutionxyz -b udc vendor/evolution
+git clone https://github.com/ManitnjG/vendor_evolutionxyz -b udc vendor/lineage
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
