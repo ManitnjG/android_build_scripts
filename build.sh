0726bc795f554d83675eca9b4bc326aaf6fb24a8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-28 12:05:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ crave run --no-patch -- "rm -rf .repo/local_manifests out
 repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests
 /opt/crave/resync.sh && \
+rm -rf vendor/evolution
+git clone https://github.com/ManitnjG/vendor_evolutionxyz -b udc vendor/evolution
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
