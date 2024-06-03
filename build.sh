dae11b1666d830ae23d8bb16b65f97cc1a8b52a8
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-06-03 11:33:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 crave run --no-patch -- "rm -rf .repo/local_manifests out
-repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
-git clone https://github.com/ManitnjG/local_manifest-1 -b 13 .repo/local_manifests
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+git clone https://github.com/ManitnjG/local_manifest-1 -b op .repo/local_manifests
 /opt/crave/resync.sh && \
-rm -rf vendor/evolution
-git clone https://github.com/ManitnjG/vendor_evolutionxyz -b udc vendor/evolution
-git clone https://github.com/ManitnjG/vendor_evolutionxyz -b udc vendor/lineage
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
-lunch lineage_X01BD-userdebug
-m evolution"
+lunch lineage_X01BD-ap1a-userdebug
+rise up"
