5ac95ea2c2e753c07dea10381aa723d3d3704617
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-06-05 17:33:53 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 -b op .repo/local_manifests
 /opt/crave/resync.sh && \
 git clone https://github.com/opudon/hardware_dolby -b 14 hardware/dolby
-git clone https://gitlab.com/maitreyapatni25/vendor_oneplus_firmware-udon.git -b 14 vendor/oneplus/firmware-udon
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
