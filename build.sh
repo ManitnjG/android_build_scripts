92043f74ebcb5514fefa43be229860e6e3f5aa3e
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-06-05 17:44:01 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@ repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/ManitnjG/local_manifest-1 -b op .repo/local_manifests
 /opt/crave/resync.sh && \
 git clone https://github.com/opudon/hardware_dolby -b 14 hardware/dolby
+export BUILD_USERNAME=ManitnjG ; \
+export BUILD_HOSTNAME=crave ; \
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
 export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
 lunch lineage_udon-ap1a-userdebug
+ make installclean
 rise b"
