e9b40968a82bdadd66000e445198da99561ad179
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2026-04-27 16:00:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ crave run --no-patch -- "rm -rf .repo/local_manifests out
 
 git clone https://github.com/ManitnjG/local_manifest-1 -b op .repo/local_manifests
 /opt/crave/resync.sh && \
-git clone https://github.com/opudon/hardware_dolby -b 14 hardware/dolby
 export BUILD_USERNAME=ManitnjG ; \
 export BUILD_HOSTNAME=crave ; \
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
