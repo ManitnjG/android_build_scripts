011a1f1ad5a54d88416c8b5a1d544aee5ecaeaed
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2026-04-27 16:53:10 +0530

    Update build-clean.sh

--- build-clean.sh
+++ build-clean.sh
@@ -1,58 +1,19 @@
 #!/bin/bash
 
 set -e
-#Credit to Meghthedev for the initial script 
 
-# Initialize repo with specified manifest
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-
-# Run inside foss.crave.io devspace, in the project folder
-# Remove existing local_manifests
 crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
 
 # Initialize repo with specified manifest
-repo init --depth 1 -u https://github.com/sounddrill31/plros_manifests.git -b lineage-20.0 --git-lfs && \
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs && \
 
-# Clone local_manifests repository
-git clone https://github.com/sounddrill31/local_manifests --depth 1 -b plrOS-oxygen .repo/local_manifests && \
-
-# Sync the repositories
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
-
-# Clone Cromite app
-rm -rf vendor/plros/prebuilt/apps/Cromite;
-git clone https://gitlab.com/plros-lab/android_packages_apps_Cromite.git vendor/plros/prebuilt/apps/Cromite && \
-
-# Set up build environment
+git clone https://github.com/ManitnjG/local_manifest-1 -b op .repo/local_manifests
+/opt/crave/resync.sh && \
+export BUILD_USERNAME=ManitnjG ; \
+export BUILD_HOSTNAME=crave ; \
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true && \
+export ALLOW_MISSING_DEPENDENCIES=true && \
 source build/envsetup.sh && \
-
-# Lunch configuration
-lunch lineage_oxygen-userdebug && \
-
-# Build the ROM
-mka bacon && \
-echo "Date and time:" && \
-
-# Print out/build_date.txt
-cat out/build_date.txt; echo \
-
-# Print SHA256
-sha256sum out/target/product/*/*.zip"
-
-# Clean up
-rm -rf oxygen/
-
-
-
-# Pull generated zip files
-crave pull out/target/product/*/*.zip 
-
-# Pull generated img files
-crave pull out/target/product/*/*.img
-
-# Upload zips to Telegram
-telegram-upload --to sdreleases oxygen/*.zip oxygen/recovery.img
-
-#Upload to Github Releases
-#cd oxygen/
-#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
+lunch lineage_udon-userdebug
+ make installclean
+mka bacon"
